ef45619381ec411317c653603f1a72c473d1abe9
thadathilsinan <thadathilsinan@gmail.com>
2024-11-28 10:53:46 +0530

    Pinned appbar buttons in home page

--- lib/features/notes/view/widgets/app_bar_buttons_widget.dart
+++ lib/features/notes/view/widgets/app_bar_buttons_widget.dart
@@ -13,45 +13,75 @@ class AppBarButtonsWidgets extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    /// Open the drawer
-    void openDrawer() {
-      scaffoldKey.currentState!.openDrawer();
-    }
+    return SliverPersistentHeader(
+      delegate: AppBarButtonsDelegate(
+        scaffoldKey: scaffoldKey,
+        titleOpacity: titleOpacity,
+      ),
+      pinned: true,
+    );
+  }
+}
 
-    return SliverToBoxAdapter(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 16),
-        child: Row(
-          children: [
-            IconButton(
-              icon: SvgPicture.asset(
-                'assets/icons/drawer.svg',
-                height: 20,
-                colorFilter:
-                    const ColorFilter.mode(Colors.black, BlendMode.srcIn),
-              ),
-              onPressed: openDrawer,
+class AppBarButtonsDelegate extends SliverPersistentHeaderDelegate {
+  final GlobalKey<ScaffoldState> scaffoldKey;
+  double titleOpacity;
+
+  AppBarButtonsDelegate(
+      {required this.scaffoldKey, required this.titleOpacity});
+
+  /// Open the drawer
+  void openDrawer() {
+    scaffoldKey.currentState!.openDrawer();
+  }
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 16),
+      child: Row(
+        children: [
+          IconButton(
+            icon: SvgPicture.asset(
+              'assets/icons/drawer.svg',
+              height: 20,
+              colorFilter:
+                  const ColorFilter.mode(Colors.black, BlendMode.srcIn),
             ),
-            Expanded(
-              child: Opacity(
-                opacity: titleOpacity,
-                child: const Text(
-                  'All notes',
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.w500,
-                  ),
+            onPressed: openDrawer,
+          ),
+          Expanded(
+            child: Opacity(
+              opacity: titleOpacity,
+              child: const Text(
+                'All notes',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w500,
                 ),
               ),
             ),
-            IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
-            IconButton(
-              icon: const Icon(Icons.more_vert),
-              onPressed: () {},
-            ),
-          ],
-        ),
+          ),
+          IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
+          IconButton(
+            icon: const Icon(Icons.more_vert),
+            onPressed: () {},
+          ),
+        ],
       ),
     );
+  }
+
+  @override
+  double get maxExtent => 80;
+
+  @override
+  double get minExtent => 80;
+
+  @override
+  bool shouldRebuild(AppBarButtonsDelegate oldDelegate) {
+    return titleOpacity != oldDelegate.titleOpacity ||
+        scaffoldKey != oldDelegate.scaffoldKey;
   }
 }
